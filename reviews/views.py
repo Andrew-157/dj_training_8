d27c3e4ff4040151137923ec4b30dded585cc490
Andrew Subotin <subotinandrey5@gmail.com>
2023-11-14 11:25:18 +0100

    feat: dummy form in the reviews app

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def review(request: HttpRequest) -> HttpResponse:
+    return render(request=request, template_name="reviews/review.html")
