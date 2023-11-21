cddbd0d7abdd0be1abe13cd1ab0e4b7982426e19
Andrew Subotin <subotinandrey5@gmail.com>
2023-11-21 20:20:42 +0100

    redirect on POST request

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,15 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
 def review(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        entered_username = request.POST['username']
+        print(entered_username)
+        return HttpResponseRedirect("/thank-you")
+
     return render(request=request, template_name="reviews/review.html")
+
+
+def thank_you(request: HttpRequest) -> HttpResponse:
+    return render(request, 'reviews/thank_you.html')
