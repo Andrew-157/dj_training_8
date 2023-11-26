db627d890fd08b4ff2cd43c3526016ab34bc147e
Andrew Subotin <subotinandrey5@gmail.com>
2023-11-26 09:20:47 +0100

    feat: saving Review model data through the ReviewForm

--- reviews/views.py
+++ reviews/views.py
@@ -2,6 +2,7 @@ from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 from .forms import ReviewForm
+from .models import Review
 
 
 def review(request: HttpRequest) -> HttpResponse:
@@ -11,7 +12,11 @@ def review(request: HttpRequest) -> HttpResponse:
     elif request.method == 'POST':
         form = ReviewForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            review = Review(
+                user_name=form.cleaned_data['user_name'],
+                review_text=form.cleaned_data['review_text'],
+                rating=form.cleaned_data['rating'])
+            review.save()
             return HttpResponseRedirect('/thank-you')
 
     return render(request, 'reviews/review.html', {'form': form})
