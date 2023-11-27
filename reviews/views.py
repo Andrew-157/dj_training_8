920b928ae4a01e466ef7a600713e752c915049b9
Andrew Subotin <subotinandrey5@gmail.com>
2023-11-27 21:36:54 +0100

    feat: reviews list using ListView

--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,11 @@
 from typing import Any
+from django.db.models.query import QuerySet
 
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 from .forms import ReviewForm
 from .models import Review
@@ -32,13 +34,15 @@ class ThankYouView(TemplateView):
         return context
 
 
-class ReviewsListView(TemplateView):
+class ReviewsListView(ListView):
     template_name = 'reviews/review_list.html'
+    model = Review
+    context_object_name = 'reviews'
 
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        context['reviews'] = Review.objects.all()
-        return context
+    def get_queryset(self) -> QuerySet[Any]:
+        base_query = super().get_queryset()
+        data = base_query.filter(rating__gte=4)
+        return data
 
 
 class SingleReviewView(TemplateView):
