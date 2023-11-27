b0fc0725dd369c0fa75133f160338370cd6f1517
Andrew Subotin <subotinandrey5@gmail.com>
2023-11-27 21:06:26 +0100

    outputing all reviews using TemplateView and get_context_data method

--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,12 @@
+from typing import Any
+
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 
 from .forms import ReviewForm
+from .models import Review
 
 
 class ReviewView(View):
@@ -22,3 +25,17 @@ class ReviewView(View):
 
 class ThankYouView(TemplateView):
     template_name = 'reviews/thank_you.html'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['message'] = "This works!"
+        return context
+
+
+class ReviewsListView(TemplateView):
+    template_name = 'reviews/review_list.html'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['reviews'] = Review.objects.all()
+        return context
